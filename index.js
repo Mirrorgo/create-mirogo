17a2ebebee9e0a3f5fa2346fdf66c914a37f028b
mirrorgo <cloudmirrorgo@163.com>
2024-05-10 13:30:03 +1000

    feat: read files

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
+const fs = require("fs");
+const path = require("path");
 
-console.log("hello nihao happy");
+const componentsDir = path.join(__dirname, "components");
+
+fs.readdir(componentsDir, (err, files) => {
+  if (err) {
+    console.error("Error reading directory:", err);
+    return;
+  }
+
+  files.forEach((file) => {
+    console.log(file);
+  });
+});
