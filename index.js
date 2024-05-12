e6ea8bec22749779e5601ea475f55bd29ced6b8b
mirrorgo <cloudmirrorgo@163.com>
2024-05-12 12:13:17 +1000

    feat: use zx

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 #!/usr/bin/env node
-const fs = require("fs");
-const path = require("path");
+import * as fs from "fs";
+import * as path from "path";
+import * as readline from "readline";
+import chalk from "chalk";
 
-const componentsDir = path.join(process.cwd(), "components");
-
-fs.readdir(componentsDir, (err, files) => {
-  if (err) {
-    console.error("Error reading directory:", err);
-    return;
-  }
-
-  files.forEach((file) => {
-    console.log(file);
-  });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
+
+rl.question(
+  chalk.yellow("Do you want to create a moodboard? (yes/no): "),
+  (answer) => {
+    if (answer.toLowerCase() === "yes") {
+      console.log(chalk.green("Creating moodboard..."));
+      // 这里可以添加创建 moodboard 的逻辑
+    } else if (answer.toLowerCase() === "no") {
+      const componentsDir = path.join(process.cwd(), "components");
+
+      fs.readdir(componentsDir, (err, files) => {
+        if (err) {
+          console.error(chalk.red("Error reading directory:"), err);
+          return;
+        }
+
+        console.log(chalk.green("Files in components directory:"));
+        files.forEach((file) => {
+          console.log(chalk.blue(file));
+        });
+      });
+    } else {
+      console.log(chalk.red("Invalid answer. Please answer 'yes' or 'no'."));
+    }
+
+    rl.close();
+  }
+);
