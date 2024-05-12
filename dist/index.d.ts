a30f1d61eb9298bcb885ea26f7738b4d24e775f4
mirrorgo <cloudmirrorgo@163.com>
2024-05-12 16:51:41 +1000

    feat: use prompts

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+export * from "/home/mirrorgo/released/create-mirogo/src/index";
+export { default } from "/home/mirrorgo/released/create-mirogo/src/index";
---
+++ dist/index.mjs
@@ -0,0 +1,13 @@
+#!/usr/bin/env zx
+import jiti from "file:///home/mirrorgo/released/create-mirogo/node_modules/.pnpm/jiti@1.21.0/node_modules/jiti/lib/index.js";
+
+/** @type {import("/home/mirrorgo/released/create-mirogo/src/index")} */
+const _module = jiti(null, {
+  "esmResolve": true,
+  "interopDefault": true,
+  "alias": {
+    "create-mirogo": "/home/mirrorgo/released/create-mirogo"
+  }
+})("/home/mirrorgo/released/create-mirogo/src/index.mjs");
+
+export default _module;
